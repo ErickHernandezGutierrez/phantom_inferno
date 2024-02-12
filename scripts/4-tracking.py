c6762581925af4f59696ef8e5d01a4edfc9fb467
ErickHernandezGutierrez <erick.hernandez@cimat.mx>
2024-02-12 10:28:49 -0500

    merge tracking and segmenting scripts

--- scripts/4-tracking.py
+++ scripts/4-tracking.py
@@ -8,6 +8,7 @@ parser.add_argument('--modsel', default='bic', help='model selector (aic,bic,fte
 parser.add_argument('--mask', help='mask filename')
 parser.add_argument('--nsubjects', default=1, type=int, help='number of subjects in the study. default: 1')
 parser.add_argument('--ntracks', default=100000, type=int, help='number of selected streamlines for the tractogram. default: 100000')
+parser.add_argument('--roi', help='ROIs filename')
 args = parser.parse_args()
 
 study = args.study_path
@@ -16,6 +17,7 @@ modsel = args.modsel
 mask = args.mask
 nsubjects = args.nsubjects
 ntracks = args.ntracks
+roi = args.roi
 
 for i in range(nsubjects):
     subject = 'sub-%.3d_ses-1' % (i+1)
@@ -23,3 +25,7 @@ for i in range(nsubjects):
 
     print('│   ├── Fiber tracking')
     os.system('tckgen %s/%s/mrds/results_MRDS_Diff_%s_ODF_SH.nii.gz %s/%s/mrds/results_MRDS_Diff_%s_TRACKS.tck -select %d -seed_image %s -grad %s -force -quiet' % (study,subject,modsel.upper(),study,subject,modsel.upper(),ntracks,mask,scheme))
+
+    print('│   ├── Segmenting Tractogram')
+    os.system('tck2connectome %s/%s/mrds/results_MRDS_Diff_%s_TRACKS.tck %s %s/%s/mrds/results_MRDS_Diff_%s_CONNECTOME.csv -assignment_radial_search 0.5 -out_assignments %s/%s/mrds/results_MRDS_Diff_%s_NODES.txt -force -quiet' % (study,subject,modsel.upper(),roi,study,subject,modsel.upper(),study,subject,modsel.upper()))
+    os.system('connectome2tck %s/%s/mrds/results_MRDS_Diff_%s_TRACKS.tck %s/%s/mrds/results_MRDS_Diff_%s_NODES.txt %s/%s/mrds/results_MRDS_Diff_%s_BUNDLES -files per_edge -force -quiet' % (study,subject,modsel.upper(),study,subject,modsel.upper(),study,subject,modsel.upper()))
