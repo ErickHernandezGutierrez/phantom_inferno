7bb4727fb636d3970e8f09631b9babfe4fe0d93c
ErickHernandezGutierrez <erick.hernandez@cimat.mx>
2024-03-18 00:55:11 -0400

    new scheme generator script

--- scripts/schemegen.py
+++ scripts/schemegen.py
@@ -1,47 +1,44 @@
 import numpy as np
-import nibabel as nib
+import argparse
 
-def read_points(filename):
+def read_points(filename, npoints):
     points_file = open(filename, 'rt')
 
     lines = points_file.readlines()
 
-    ndirs = int(lines[0])
+    points = np.zeros((npoints,3), dtype=np.float32)
 
-    points = np.zeros((ndirs,3), dtype=np.float32)
-
-    for i in range(ndirs):
-        x = float(lines[1 + 3*i])
-        y = float(lines[1 + 3*i+1])
-        z = float(lines[1 + 3*i+2])
-
+    for i in range(npoints):
+        line = lines[i].split(' ')
+        x,y,z = float(line[0]),float(line[1]),float(line[2])
         points[i, :] = np.array([x,y,z])
 
     return points
 
+parser = argparse.ArgumentParser(description='Generate scheme.')
+parser.add_argument('out_file', help='output scheme filename')
+parser.add_argument('--bvals', type=int, nargs='*', help='list of the b-values')
+parser.add_argument('--ndirs', type=int, nargs='*', help='list of the number of directions for each b-value')
+parser.add_argument('--nb0s', type=int, default=7, help='number of b0s. [7]')
+args = parser.parse_args()
+
+out_file = args.out_file
+bvals = args.bvals
+ndirs = args.ndirs
+nb0s = args.nb0s
+
 scheme = []
 
-for i in range(7):
+for i in range(nb0s):
     scheme.append( np.array([0,0,0,0]) )
 
-for bval in range(100, 1000, 200):
-    points = read_points('../data/Elec10.txt')
+for (bval,n) in zip(bvals,ndirs):
+    points = read_points('data/points/PuntosElectroN%d.txt'%n, n)
     for point in points:
         scheme.append( np.array([point[0], point[1], point[2], bval]) )
 
-for bval in range(1000, 2000, 200):
-    points = read_points('../data/Elec30.txt')
-    for point in points:
-        scheme.append( np.array([point[0], point[1], point[2], bval]) )
-
-for bval in range(2000, 3200, 200):
-    points = read_points('../data/Elec60.txt')
-    for point in points:
-        scheme.append( np.array([point[0], point[1], point[2], bval]) )
-
-scheme = np.array(scheme)
-
-with open('scheme.txt', 'wt') as scheme_file:
+# save scheme to file
+with open(out_file, 'wt') as scheme_file:
     for line in scheme:
         scheme_file.write('%f %f %f %f\n' % (line[0], line[1], line[2], line[3]))
 
